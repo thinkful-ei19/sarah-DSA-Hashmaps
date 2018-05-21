0493c3926d81af067d74feb4e8453a76617beaf2
ssheaks <sarah.sheaks@gmail.com>
2018-05-21 09:12:42 -0700

    create lor hashmap

--- hashmaps.js
+++ hashmaps.js
@@ -83,4 +83,22 @@ class HashMap {
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
+function main() {
+  const lor = new HashMap();
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('Ringbearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+  console.log(lor);
+}
+main()
+
 module.exports = HashMap;
+
